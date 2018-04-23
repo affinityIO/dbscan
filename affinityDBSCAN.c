d55ecd92451b9ec74e44ea8dd4d6814edb2105c7
affinityIO <you@example.com>
2018-04-23 09:19:02 -0700

    First compiled

---
+++ affinityDBSCAN.c
@@ -0,0 +1,33 @@
+// Joseph Moreno
+// CSC 251
+// 4/23/18
+// DBSCAN
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+double stdDistance(void);
+void rangeQuery(void);
+int main(void)
+{
+    int c, i;
+    FILE *dataPointer;
+
+
+    return 0; 
+}
+
+double stdDistance()
+{
+
+}
+
+void rangeQuery()
+{
+    
+}
+
+
